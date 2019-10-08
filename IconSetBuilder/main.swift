2b416df26300dc61014fc22d2ddb1527e2246925
Michael Salmon <msalmonse@gmail.com>
2019-10-08 09:40:28 +0200

    Initial Commit

---
+++ IconSetBuilder/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  IconSetBuilder
+//
+//  Created by Michael Salmon on 2019-10-08.
+//  Copyright © 2019 mesme. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
